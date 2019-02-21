94d189a1bea45533958d461e41ab878ea16890a9
Raphael Almeida <raphael.rb96@gmail.com>
2019-02-21 13:37:49 -0400

    VideoPlayer implementação fase1

---
+++ Museu/Assets/StreamVideo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StreamVideo : MonoBehaviour {
+
+	public RawImage rawImage;
+	public VideoPlayer videoPlayer;
+	public VideoSource VideoSource;
+	//public VideoClip videoToPlay;
+	public AudioSource audioSource;
+
+	public void PlayPause(){
+		if (!videoPlayer.isPlaying) {
+			videoPlayer.Play ();
+			audioSource.Play ();
+			rawImage.enabled = false;
+			print ("play");
+		} else {
+			videoPlayer.Pause ();
+			audioSource.Pause ();
+			rawImage.enabled = true;
+			print ("pause");
+		}
+	}
+}
