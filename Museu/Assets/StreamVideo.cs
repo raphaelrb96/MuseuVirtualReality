1b334ea573acad3f3bae50f62164f8920f59dd10
Raphael Almeida <raphael.rb96@gmail.com>
2019-02-23 12:37:53 -0400

    videoplayer e navegacao

--- Museu/Assets/StreamVideo.cs
+++ Museu/Assets/StreamVideo.cs
@@ -7,22 +7,25 @@ using UnityEngine.Video;
 public class StreamVideo : MonoBehaviour {
 
 	public RawImage rawImage;
+	public Image icone;
 	public VideoPlayer videoPlayer;
 	public VideoSource VideoSource;
 	//public VideoClip videoToPlay;
 	public AudioSource audioSource;
+
 
 	public void PlayPause(){
 		if (!videoPlayer.isPlaying) {
 			videoPlayer.Play ();
 			audioSource.Play ();
 			rawImage.enabled = false;
-			print ("play");
+			icone.enabled = false;
 		} else {
 			videoPlayer.Pause ();
 			audioSource.Pause ();
 			rawImage.enabled = true;
-			print ("pause");
+			icone.enabled = true;
 		}
 	}
+		
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MuseuVirtualReality
+
+Este projeto faz parte do programa Udacity VR Developer Nanodegree. A finalidade deste projeto foi aprensentar: uma breve historia da realidade virtual, e mostrar possiveis areas de atuação. 
+Essa Documentação mostra detalhes de desenvolvimento e usabilidade.
+
+### Propósito
+Museu é uma experiencia projetada para Realidade virtual com a finalidade de mostrar a quem está tendo suas primeiras experiencias em Realidade virtual, de onde surgiu surgiu essa plataforma e quais areas ela pode impactar futuramente.
